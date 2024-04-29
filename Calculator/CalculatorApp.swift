1f151c5b10d9f8ced3bb1d05b2c9fa0b1a08f57e
Students <students@SAITMs-iMac-9.local>
2024-04-29 14:18:51 +0530

    swift first project

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Students on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
